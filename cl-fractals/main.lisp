a5e14ed6fe776211c07fb85147d4849c08c52d3e
taptap <alexey.e.egorov@gmail.com>
2013-03-18 12:39:23 +0400

    implemented axioms list, added function to add axioms to list

--- cl-fractals/main.lisp
+++ cl-fractals/main.lisp
@@ -4,37 +4,39 @@
 
 (in-package :cl-fractals)
 
+(defvar *axiom-widgets* '())
+
+(defun add-axiom (listbox list text)
+    (listbox-append listbox text)
+    (setf list (cons text list)))
+
+
 (defun create-window ()
   (with-ltk ()
     (let* ((c (make-instance 'canvas :borderwidth 2 :relief :sunken))
 	   (th (make-instance 'entry :text "theorem"))
-	   (f (make-instance 'frame :borderwidth 2 :relief :sunken))
-	   (scrll (make-instance 'scrollbar :master f))
-	   (axi (make-instance 'entry  :text "axiom" :master f))
-	   (add (make-instance 'button :text "Add" :master f))
-	   (del (make-instance 'button :text "Delete" :master f))
+	   (f (make-instance 'frame :relief :sunken
+			     :borderwidth 2))
+	   (lb (make-instance 'listbox :master f))
+	   (scrll (make-instance 'scrollbar :orientation :vertical :master f))
+	   (add (make-instance 'button :text "Add"))
+	   (del (make-instance 'button :text "Delete"))
 	   (dpth (make-instance 'entry :text "Depth"))
 	   (plot (make-instance 'button :text "Plot"))
 	   (quit (make-instance 'button :text "Quit")))
+      ;; add scrollbar to listbox
+      (configure scrll "command" (concatenate 'string (widget-path lb) " yview"))
+      (configure lb "yscrollcommand" (concatenate 'string (widget-path scrll) " set"))
+      ;; canvas and theorem entry
       (grid c 0 0 :rowspan 5)
-      (grid th 0 1 :columnspan 2)
-;;      (pack f)
-;;      (pack axi)
-;;      (pack scrll :side :right)
-;;      (pack add :side :left)
-;;      (pack del :side :left)
-      (grid f 1 1 :rowspan 3 :columnspan 2 :sticky "n")
-      (grid axi 1 1 :columnspan 2)
-      (grid add 2 1)
-      (grid del 2 2)
-      (grid dpth 3 1 :columnspan 2 :sticky "n")
-      (grid plot 4 1 :sticky "n")
-      (grid quit 4 2 :sticky "n"))))
-		    
-
-
-(defun hello-1 ()
-  (with-ltk ()
-    (let ((button (make-instance 'button :text "A Button")))
-      ;; Place BUTTON at x=50, y=40
-      (place button 50 40 :width 100 :height 50) )))
+      (grid th 0 1 :columnspan 2 :padx 4 :sticky "we")
+      ;; frame with axioms list and a scrollbar
+      (grid f 1 1 :columnspan 2)
+      (pack scrll :side :right :fill :y)
+      (pack lb :side :left)
+      ;; remaining widgets
+      (grid add 2 1 :sticky "we")
+      (grid del 2 2 :sticky "we")
+      (grid dpth 3 1 :columnspan 2 :padx 4 :sticky "we")
+      (grid plot 4 1)
+      (grid quit 4 2))))
