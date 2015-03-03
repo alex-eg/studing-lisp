6dd2721ed7e263e4b2e6b3df8a837668ddbfbdd8
taptap <alexey.e.egorov@gmail.com>
2015-03-03 21:23:41 +0300

    wip

--- cffi-test/test.lisp
+++ cffi-test/test.lisp
@@ -3,7 +3,8 @@
     (asdf:load-system :cffi)
     (defpackage #:cffi-curl-test
       (:use #:cl
-            #:cffi))))
+            #:cffi)
+      (:export #:test-curl))))
 
 (in-package #:cffi-curl-test)
 
@@ -12,58 +13,40 @@
 
 (use-foreign-library libcurl)
 
-;; CURLcode curl_global_init(long flags);
 (defctype curl-code :int)
+(defctype easy-handle :pointer)
+(defctype size :unsigned-int)
+
+(defparameter *easy-handle* nil)
+(defvar *easy-handle-cstrings* (make-hash-table))
+(defvar *easy-handle-errorbuffers* (make-hash-table))
+(defparameter *curl-error-size* 257)
+
 (defcfun "curl_global_init" curl-code
   (flags :long))
 
-;; CURL *curl_easy_init( );
 (defcfun "curl_easy_init" :pointer)
 
-;; void curl_easy_cleanup(CURL *handle);
 (defcfun "curl_easy_cleanup" :void
   (easy-handle :pointer))
 
-(defparameter *easy-handle* nil)
+(defcfun "curl_easy_perform" curl-code
+  (handle easy-handle))
 
-;; CURLcode curl_easy_setopt(CURL *curl, CURLoption option, ...);
-;;
-;; #define CURLOPTTYPE_LONG          0
-;; #define CURLOPTTYPE_OBJECTPOINT   10000
-;; #define CURLOPTTYPE_FUNCTIONPOINT 20000
-;; #define CURLOPTTYPE_OFF_T         30000
-;;
-;; #define LONG          CURLOPTTYPE_LONG
-;; #define OBJECTPOINT   CURLOPTTYPE_OBJECTPOINT
-;; #define FUNCTIONPOINT CURLOPTTYPE_FUNCTIONPOINT
-;; #define OFF_T         CURLOPTTYPE_OFF_T
-;; #define CINIT(name,type,number) CURLOPT_/**/name = type + number
-;; e.g.
-;; /* "user:password" to use with proxy. */
-;; CINIT(PROXYUSERPWD, OBJECTPOINT, 6),
-;; /* Range to get, specified as an ASCII string. */
-;; CINIT(RANGE, OBJECTPOINT, 7),
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun add-curl-handle-cstring (handle cstring)
+    (car (push cstring (gethash handle *easy-handle-cstrings*))))
 
-(defctype easy-handle :pointer)
-
-(defmacro curl-easy-setopt (easy-handle enumerated-name
-                            value-type new-value)
-  `(foreign-funcall "curl_easy_setopt"
-                    easy-handle ,easy-handle
-                    curl-option ,enumerated-name
-                    ,value-type ,new-value
-                    curl-code))
-
-(defun curry-curl-option-setter (fun-name opt-kw)
-  (setf (symbol-function fun-name)
-        (let ((c-fun (symbol-function fun-name)))
-          (lambda (easy-handle new-value)
-            (funcall c-fun easy-handle opt-kw
-                     (if (stringp new-value)
-                         (add-curl-handle-cstring
-                          easy-handle
-                          (foreign-string-alloc new-value))
-                         new-value))))))
+  (defun curry-curl-option-setter (fun-name opt-kw)
+    (setf (symbol-function fun-name)
+          (let ((c-fun (symbol-function fun-name)))
+            (lambda (easy-handle new-value)
+              (funcall c-fun easy-handle opt-kw
+                       (if (stringp new-value)
+                           (add-curl-handle-cstring
+                            easy-handle
+                            (foreign-string-alloc new-value))
+                           new-value)))))))
 
 (defmacro define-curl-option-setter (name option-type
                                      option-value foreign-type)
@@ -100,61 +83,55 @@
                                           (off-t :long)))))
        ',type-name)))
 
-(define-curl-options curl-option
-    (long 0 objectpoint 10000 functionpoint 20000 off-t 30000)
-  (:noprogress long 43)
-  (:nosignal long 99)
-  (:errorbuffer objectpoint 10)
-  (:url objectpoint 2)
-  (:writefunction functionpoint 11))
-
-(defvar *easy-handle-cstrings* (make-hash-table))
-(defvar *easy-handle-errorbuffers* (make-hash-table))
-
-(defparameter *curl-error-size* 257)
-
-(defun make-easy-handle ()
-  (let ((easy-handle (curl-easy-init)))
-    (setf (gethash easy-handle *easy-handle-cstrings*) '())
-    (setf (gethash easy-handle *easy-handhle-errorbuffers*)
-          (foreign-alloc :char
-                         :count *curl-error-size*
-                         :initial-element 0))
-    easy-handle))
-
 (defun free-easy-handle (handle)
   (curl-easy-cleanup handle)
   (foreign-free (gethash handle *easy-handle-errorbuffers*))
   (mapc #'foreign-string-free
         (gethash handle *easy-handle-cstrings*))
   (remhash handle *easy-handle-cstrings*)
+  (remhash handle *easy-handle-errorbuffers*)
   (setf handle nil))
 
 (defun get-easy-handle-error (handle)
   (foreign-string-to-lisp
    (gethash handle *easy-handle-errorbuffers*)))
 
-(defun add-curl-handle-cstring (handle cstring)
-  (car (push cstring (gethash handle *easy-handle-cstrings*))))
+(eval-when (:compile-toplevel)
+  (define-curl-options curl-option
+      (long 0 objectpoint 10000 functionpoint 20000 off-t 30000)
+    (:noprogress long 43)
+    (:nosignal long 99)
+    (:errorbuffer objectpoint 10)
+    (:url objectpoint 2)
+    (:writefunction functionpoint 11)))
 
-(defctype size :unsigned-int)
+(defun make-easy-handle ()
+  (let ((easy-handle (curl-easy-init)))
+    (setf (gethash easy-handle *easy-handle-cstrings*) '())
+    (setf (gethash easy-handle *easy-handle-errorbuffers*)
+          (foreign-alloc :char
+                         :count *curl-error-size*
+                         :initial-element 0))
+    (set-curl-option-errorbuffer easy-handle
+                                 (gethash
+                                  easy-handle
+                                  *easy-handle-errorbuffers*))
+    easy-handle))
 
-(defcallback easy-write size
-    ((ptr :pointer)
-     (size size)
-     (nmemb size)
-     (stream :pointer))
-  (declare (ignore stream))
-  (let ((data-size (* size nmemb)))
-    (handler-case
-        (progn (funcall (symbol-value '*easy-write-procedure*)
-                        (foreign-string-to-lisp ptr data-size nil))
-               data-size)
-      (error () (if (zerop data-size) 1 0)))))
-
-(defcfun "curl_easy_perform" curl-code
-  (handle easy-handle))
-
-
-
-
+(defun test-curl (url)
+  (setf *easy-handle* (make-easy-handle))
+  (set-curl-option-nosignal *easy-handle* 1)
+  (set-curl-option-url *easy-handle* url)
+  (set-curl-option-writefunction *easy-handle*
+                                 (callback easy-write))
+  (with-output-to-string (contents)
+    (let ((*easy-write-procedure*
+           (lambda (string)
+             (write-string string contents))))
+      (declare (special *easy-write-procedure*))
+      (foreign-string-to-lisp (car (gethash *easy-handle*
+                                            *easy-handle-cstrings*)))
+      (curl-easy-perform *easy-handle*)
+      (format t "Error: ~s~%" (get-easy-handle-error *easy-handle*))
+      (free-easy-handle *easy-handle*)
+      contents)))
