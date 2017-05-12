15a273890a6ed8e134195472d232bc807e31fdcc
taptap <alexey.e.egorov@gmail.com>
2017-05-12 21:56:40 +0300

    add some more code to method combinations specimen
    It works now, but test case definition is far from perfect or even
    remotely useful.
    Points of interest: `call-method` and `make-method` (which is not yet
    represented here) are special macros local to
    `define-method-combination` form. Documentation on them available
    online in the Hyperspec.
    
    Replacing `call-next-method` with just `collide` inside :test
    specified method causes Lisp to exhaust the stack (quite obviously).
    
    Assert reports failure in a form of runtime error. So it may be useful
    to replace it with some package-defined function to report cases
    nicely.
    
    Also if I proceed to use method testing as example, it would be nice
    to add some convenience macros for defining test methods or even
    define-method-with-test or something like this.

--- method-combinations/mc.lisp
+++ method-combinations/mc.lisp
@@ -1,8 +1,56 @@
 (defpackage :mc
-  (:use :cl))
+  (:use :cl)
+  (:export test
+           test-test
+           *test*))
 
 (in-package :mc)
 
-(define-method-combination test
-    ((primary () :required t)
-     (test )))
+(defvar *test* nil)
+
+(define-method-combination test ()
+  ((primary () :required t)
+   (test (:test)))
+  (if *test*
+      `(call-method ,(car test) ,(list (car primary)))
+      `(call-method ,(car primary))))
+
+
+(defclass ship ()
+  ((health :initarg :health
+           :accessor ship-health)
+   (destroyed :initform nil
+              :accessor ship-destroyed)))
+
+(defclass asteroid ()
+  ((mass :initarg :mass
+         :reader asteroid-mass)))
+
+(defgeneric collide (a b)
+  (:method-combination test))
+
+
+(defmethod collide ((s ship) (a asteroid))
+  (let* ((h (ship-health s))
+        (new-health (- h (asteroid-mass a))))
+    (if (not (plusp new-health))
+        (setf (ship-health s) 0
+              (ship-destroyed s) t)
+        (setf (ship-health s) new-health))))
+
+(defmethod collide :test ((s1 ship) (a1 asteroid))
+  (declare (ignore s1 a1))
+  (let ((s (make-instance 'ship :health 100))
+        (a (make-instance 'asteroid :mass 50)))
+    (call-next-method s a)
+    (assert (= 50 (ship-health s)))))
+
+(defun test ()
+  (let ((s (make-instance 'ship :health 20))
+        (a (make-instance 'asteroid :mass 10)))
+    (collide s a)
+    (format t "Ship now has ~a health" (ship-health s))))
+
+(defun test-test ()
+  (let ((*test* t))
+    (collide (make-instance 'ship) (make-instance 'asteroid))))
